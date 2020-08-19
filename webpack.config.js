6cbcd81ba812d43a1adfec1888b95d68848fd059
Victorio Berra <tory.berra@wustl.edu>
2020-08-19 09:22:35 -0500

    Checkpoint before going another direction with banner.

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,60 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const webpack = require('webpack');
+const fs = require('fs');
+const rfr = require('rfr');
 
-module.exports = {
-  entry: './src/index.ts',
-  //devtool: 'inline-source-map', // Disabled because final build is just way too big for tampermonkey
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+const tamperMonkeyFile = fs.readFileSync(rfr.resolve('./tampermonkey.txt'), { encoding:'utf8' });
+const packageVersion = rfr("./package.json").version;
+const tampermonkeyFileHeader = tamperMonkeyFile.replace(/\@\@VERSION\@\@/, packageVersion);
+
+module.exports = (env, argv) => {
+
+  const isDev = argv.mode === "development";
+
+  const pluginsArr = [
+    new ForkTsCheckerWebpackPlugin()
+  ];
+
+  const optimization = {
+    minimize: true,
+    minimizer: []
+  }
+
+  // load plugin only in development mode
+  if (!isDev) {
+    optimization.minimizer.push(new TerserPlugin({
+      extractComments: /.*/i ///^\/\/\s\=\=UserScript\=\=.*\*\/\n\n$/gms
+    }));
+    pluginsArr.push(new webpack.BannerPlugin({
+      banner: tampermonkeyFileHeader
+    }));
+  }
+
+  return {
+    optimization: optimization,
+    entry: './src/index.ts',
+    devtool: isDev ? "inline-source-map" : "", // generate source code only in development mode
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true
+          }
+        },
+      ],
+    },
+    resolve: {
+      extensions: [ '.tsx', '.ts', '.js' ],
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: pluginsArr
+  }
 };
